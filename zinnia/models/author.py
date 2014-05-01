f688f05cd71bc024e6adb6dbd62af6f26db8e6f8
Ivan VenOsdel <ivandv@gmail.com>
2014-05-01 16:19:34 -0500

    Update the url referenced in get_absolute_url for author

--- zinnia/models/author.py
+++ zinnia/models/author.py
@@ -36,7 +36,7 @@ class Author(get_user_model(),
         """
         Builds and returns the author's URL based on his username.
         """
-        return ('zinnia_author_detail', [self.slug])
+        return ('author_detail', [self.slug])
 
     def __str__(self):
         """
